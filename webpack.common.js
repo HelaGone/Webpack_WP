ade0db587528182d8742a2b49a162c7865fa7f2a
Holkan <luah613@gmail.com>
2019-03-29 14:57:19 -0600

    minimizer css & js

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,8 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -40,7 +42,11 @@ module.exports = {
     	      		reuseExistingChunk: true
     	    	}
     	  	}
-		}
+		},
+		minimizer:[
+			new OptimizeCSSAssetsPlugin({}),
+			new TerserPlugin({})
+		]
 	},
 	module: {
 		rules:[{
