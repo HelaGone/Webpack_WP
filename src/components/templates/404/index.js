9440fbacdb17835326eb78fddb93b5d9bcc957c7
Holkan <luah613@gmail.com>
2019-03-29 15:00:42 -0600

    404 files

---
+++ src/components/templates/404/index.js
@@ -0,0 +1,10 @@
+/*General Styles*/
+import '../../../css/style.css';
+
+/*Local Styles*/
+import './not_found-styles.css';
+import './not_found-scripts.js';
+
+/*Components*/
+import '../../parts/header/index';
+import '../../parts/footer/index';
